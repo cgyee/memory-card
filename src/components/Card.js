4da6216e12fd38a821723f6b83a71245a085dc41
cgyee <cyee2017@fau.edu>
2021-01-05 21:54:43 -0500

    basic functionality completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 const Card = (props) => {
-    console.log("pprops", props)
     return (
         <div className="card">
             <img 
                 className="card--image"
-                onClick={() => props.onClick()} 
+                onClick={()=> props.onClick()}
                 src={props.value.src} 
                 alt="" 
             />
--- src/components/Gamepage.js
+++ src/components/Gamepage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import images from  '../images'
 import Card from './Card'
 
@@ -10,40 +10,58 @@ const Gamepage = () => {
             "src": image
         }
     }))
-    console.log(cards)
-    // const [bestScore, setBestScore] = useState(0)
-    // const [score, setScore] = useState(0)
+    const [bestScore, setBestScore] = useState(0)
+    const [score, setScore] = useState(0)
+    const MAX_SCORE = cards.length
+    console.log(MAX_SCORE)
 
-    /* useEffect(() => {
-        setCards(prevState => {
-            const imgs = images.map(image => {
-                return  {
-                    isSelected: false,
-                    src: image
-                }
-            })
-            return prevState.concat(imgs)
-        })  
+    useEffect(() => {
+        function updateBestScore(currentScore) {
+            (bestScore < currentScore) &&  setBestScore(currentScore)
+            alertWinner(currentScore)
+        }
         
-    }, []) */
-
-    const shuffle = () => {
-        setCards(prevState => {
-            console.log(":shuffle", prevState)
-
-            for(let i = prevState.length-1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i+1));
-                [prevState[i], prevState[j]] = [prevState[j], prevState[i]]
-                console.log("here", prevState[i], prevState[j])
-
+        function alertWinner(currentScore) {
+            if(currentScore >= MAX_SCORE) {
+                setScore(0)
             }
-            return prevState
-        })
+        }
+        updateBestScore(score)
+    }, [bestScore, score, MAX_SCORE])
+
+    const checkCard = (card) => {
+        const selectedCard = cards.filter(element => element === card)[0]
+        if(selectedCard.isSelected) {
+            setScore(0)
+            updateCards()
+
+        }
+        else {
+            setScore(prevState=> prevState + 1)
+        }
+        selectedCard.isSelected = true
+    }
+    
+    const shuffle = (array) => {
+        for(let i = array.length-1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i+1));
+            [array[i], array[j]] = [array[j], array[i]]
+        }
+        return array
 
     }
-    const display = cards.map((card, i) => <Card key={`card-${i}`} value={card} onClick={shuffle} />)
+
+    const  updateCards = () => {
+        setCards(prevState => prevState.map(item => {item.isSelected = false; return item }))
+    }
+    // [...Array] is equivalent to Array.Slice
+    const display = cards.map((card, i) => <Card key={`card-${i}`} value={card} onClick={() => {checkCard(card); setCards(prevState => shuffle([...prevState]))}} />)
     return (
         <div className="container">
+            <div>
+                <h2>Best Score: {bestScore}</h2>
+                <h2>Score: {score}</h2>
+            </div>
             <div className="card-group">
                 {display}
             </div>
